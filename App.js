a796b5bf49f57d2693f4573fbe19042b7ca231aa
Marcus-V <6114@ait.nsw.edu.au>
2021-06-23 11:59:19 +1000

    Adding dropdown component

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { StyleSheet, Text, View, TextInput, FlatList } from 'react-native';
+import {Dropdown} from "./components/Dropdown"
 
 let categories = [
   {label:"Food", value: "food"},
@@ -12,11 +13,25 @@ let categories = [
 ]
 
 export default function App() {
+  //states
+  const [category, setCategory] = useState();
+  const [ammount, setAmmount] = useState();
+
+  const dropdownChange = (value) =>{
+    //console.log(value);
+    setCategory(value);
+  }
+  const ammountChange = (value) =>{
+    //console.log(value);
+    setAmmount(value);
+  }
+
   return (
     <View style={styles.container}>
       <Text>Money Tracker</Text>
-      <View>
-        <TextInput></TextInput>
+      <View >
+        <TextInput keyboardType="number-pad" style={styles.input} onChangeText={ammountChange}></TextInput>
+        <Dropdown categories={categories} handler={dropdownChange}/>
       </View>
       <StatusBar style="auto" />
     </View>
@@ -26,7 +41,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#abdae1',
     paddingTop: 40,
+  },
+  input:{
+    backgroundColor: '#A3B9C9',
+    padding:10,
+    fontSize:20,
   },
 });
